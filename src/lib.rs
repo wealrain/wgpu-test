baed1788956a0eb2528bfd1f81bc3d20960ae1f1
zhoutaosheng <zhoutaosheng@cloudcsp.com>
2024-05-14 11:09:18 +0800

    使用索引

--- src/lib.rs
+++ src/lib.rs
@@ -47,10 +47,38 @@ impl Vertex {
     }
 }
 
+// const VERTICES: &[Vertex] = &[
+//     Vertex { position: [0.0,0.5,0.0],color: [1.0,0.0,0.0] },
+//     Vertex { position: [-0.5,-0.5,0.0],color: [0.0,1.0,0.0] },
+//     Vertex { position: [0.5,-0.5,0.0],color: [0.0,0.0,1.0] },
+// ];
+
+// const VERTICES: &[Vertex] = &[
+//     Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+//     Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+//     Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+
+//     Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+//     Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+//     Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+
+//     Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+//     Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+//     Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+// ];
+
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0,0.5,0.0],color: [1.0,0.0,0.0] },
-    Vertex { position: [-0.5,-0.5,0.0],color: [0.0,1.0,0.0] },
-    Vertex { position: [0.5,-0.5,0.0],color: [0.0,0.0,1.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 struct State {
@@ -63,6 +91,8 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -184,6 +214,13 @@ impl State {
 
         let num_vertices = VERTICES.len() as u32;
 
+        // 索引缓存区数据
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor{
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
         Self {
             surface,
             device,
@@ -193,7 +230,9 @@ impl State {
             clear_color,
             render_pipeline,
             vertex_buffer,
-            num_vertices
+            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -268,8 +307,10 @@ impl State {
             });
             render_pass.set_pipeline(&self.render_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             //告诉 wgpu 用 3 个顶点和 1 个实例（实例的索引就是 @builtin(vertex_index) 的由来）来进行绘制。
-            render_pass.draw(0..self.num_vertices,0..1);
+            // render_pass.draw(0..self.num_vertices,0..1);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
